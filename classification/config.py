17771a9c373e9153ac79d2178bcca41d0c847e9e
Eric Falkenberg <exf4789@rit.edu>
2017-04-08 18:40:56 +0000

    Added argument parsing functionality. kd_tree.py now accepts a filename and (optional) class arguments

--- classification/config.py
+++ classification/config.py
@@ -1,6 +1,24 @@
-kdtree_config = {
-    'description': """
-                    Classify isolated handwritten math symbols with a k-d tree implementation of
-                    the 1-NN algorithm.
-                   """
+## KD-Tree Specific Configuration
+kdtree_meta = {
+    'program_description': """
+                           Classify isolated handwritten math symbols with a k-d tree implementation of
+                           the 1-NN algorithm.
+                           """
+}
+kdtree_model = {
+    'leaf_size' : 40
+}
+
+## General Configuration
+dataset_config = {
+    'metavar' : 'f',
+    'type'  : str,
+    'nargs' : 1,
+    'help'  : 'The filename for the csv file that contains the dataset.'
+}
+classes_config = {
+    'metavar' : 'c',
+    'type' : str,
+    'nargs' : '*',
+    'help' : 'The class(es) that should be processed by the classifier'
 }
--- classification/kd_tree.py
+++ classification/kd_tree.py
@@ -1,18 +1,20 @@
 import argparse
-from config import kdtree_config
+from config import kdtree_meta, kdtree_model, dataset_config, classes_config
 from sklearn.neighbors import KDTree
 import numpy as np
 
 class classifier:
 
-    def __init__(this, X, Y):
+    def __init__(this, X, Y, config):
         this.X = X
         this.Y = Y
-        this.tree = KDTree(X)
+        this.tree = KDTree(X, **config)
 
 if __name__ == '__main__':
     ## Parse command line arguments
-    parser = argparse.ArgumentParser(description=kdtree_config['description']) 
+    parser = argparse.ArgumentParser(description=kdtree_meta['program_description']) 
+    parser.add_argument('dataset', **dataset_config)
+    parser.add_argument('classes', **classes_config)
     args = parser.parse_args()
     ## Run classifier
-    c = classifier(np.random.random((10, 2)), np.random.randint(0, 1))
+    c = classifier(np.random.random((10, 2)), np.random.randint(0, 1), kdtree_model)
