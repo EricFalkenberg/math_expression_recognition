166fed68da340235c9ef65a220bb74668367a1a6
Eric Falkenberg <exf4789@rit.edu>
2017-04-10 22:32:52 -0400

    Add files via upload

--- data_operations/split-class.py
+++ data_operations/split-class.py
@@ -1,59 +1,67 @@
+"""
+File: filter-class.py - Assignment 1
+Author: Ryan Tompkins
+Class: Pattern Recognition
+Prof: Professor Zanibbi
+
+Question 1 for Assignment 3
+"""
+
 import sys
 import csv
-import numpy as np
+import random
 
-def usage(error = None):
-    print "usage: python split-class.py t iso_GT junk_GT"
-    if error: print "ERROR: %s" % error
+# filenames that this script writes to
+filenames = ["junk-train.csv", "junk-test.csv",
+             "real-train.csv", "real-test.csv"]
 
+"""
+Run this script with any two CSV files as input
+"""
 def main():
-    args = sys.argv
-    if len(args) != 4:
-        usage()    
-        return
-    try:
-        t = int(args[1])
-        if not (0 < t < 100):
-            usage("ERROR: t value must be between 0 and 100")
-        ## Read real data from isoGT
-        isoGT_name = args[2]
-        with open(isoGT_name) as f:
-            csvreader = csv.reader(f)
-            arr = np.array([row for row in csvreader])
-            train = arr[np.random.randint(arr.shape[0], size=np.round(len(arr)*(100-t)/100)), :]
-            test  = arr[np.random.randint(arr.shape[0], size=np.round(len(arr)*t/100)), :] 
-        ## Write training data to real-train.csv
-        with open("real-train.csv", "w") as f:
-            csvwriter = csv.writer(f)
-            for row in train:
-                csvwriter.writerow(row)
-        ## Write testing data to real-test.csv
-        with open("real-test.csv", "w") as f:
-            csvwriter = csv.writer(f)
-            for row in test:
-                csvwriter.writerow(row)
-        ## Read junk data from junkGT 
-        junkGT_name = args[3]
-        with open(junkGT_name) as f:
-            csvreader = csv.reader(f)
-            arr = np.array([row for row in csvreader])
-            train = arr[np.random.randint(arr.shape[0], size=np.round(len(arr)*(100-t)/100)), :]
-            test  = arr[np.random.randint(arr.shape[0], size=np.round(len(arr)*t/100)), :] 
-        ## Write training data to real-train.csv
-        with open("junk-train.csv", "w") as f:
-            csvwriter = csv.writer(f)
-            for row in train:
-                csvwriter.writerow(row)
-        ## Write testing data to real-test.csv
-        with open("junk-test.csv", "w") as f:
-            csvwriter = csv.writer(f)
-            for row in test:
-                csvwriter.writerow(row)
-        
-    except ValueError as e:
-        usage(e.message)
-    except IOError as e:
-        usage(e.message)
+    args = sys.argv[1:]
+    if len(args) < 3:
+        print("Usage: python3 split-class.py <real_data_file> <junk_data> <t>")
+        sys.exit(-1)
 
-if __name__ == "__main__":
-    main()
+    realDataFile = args[0]
+    junkDataFile = args[1]
+    randomT      = float(args[2]) / 100
+
+    realData      = []   # this will be used as the training data later
+    junkData      = []
+
+    with open(realDataFile) as f:
+        f_csv = csv.reader(f)
+        numSamples = 0
+        for row in f_csv:
+            numSamples = numSamples + 1
+            realData.append([row[0], row[1]])
+
+    realTestData = random.sample(realData, round(randomT * numSamples))
+
+    with open(junkDataFile) as f:
+        f_csv = csv.reader(f)
+        numSamples = 0
+        for row in f_csv:
+            numSamples = numSamples + 1
+            junkData.append([row[0], row[1]])
+
+    junkTestData = random.sample(junkData, round(randomT * numSamples))
+
+    # remove the test samples from the data sets to use them as the training data
+    for i in range(0, len(realTestData)):
+        realData.remove(realTestData[i])
+    for i in range(0, len(junkTestData)):
+        junkData.remove(junkTestData[i])
+
+    dataList = [junkData, junkTestData, realData, realTestData]
+    filenames = ["junk-train.csv", "junk-test.csv",
+                 "real-train.csv", "real-test.csv"]
+
+    for i in range(len(dataList)):
+        with open(filenames[i], "w") as f:
+            f_csv = csv.writer(f)
+            f_csv.writerows(dataList[i])
+
+main()
