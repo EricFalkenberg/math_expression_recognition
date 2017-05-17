857043580750b41a5a53d80cfe16264127fb708f
Eric Falkenberg <exf4789@rit.edu>
2017-05-17 04:15:34 -0400

    Started implementation of visual features for involved segmenter. Commented out some of the training data so that testing is quick

--- source/config.py
+++ source/config.py
@@ -1,11 +1,11 @@
 file_handler_config = {
     'training_data_loc' : [
-            "/home/eric/Desktop/pattern_rec/TrainINKML/expressmatch",
-            "/home/eric/Desktop/pattern_rec/TrainINKML/extension",
-            "/home/eric/Desktop/pattern_rec/TrainINKML/HAMEX",
-            "/home/eric/Desktop/pattern_rec/TrainINKML/MathBrush",
-            "/home/eric/Desktop/pattern_rec/TrainINKML/MfrDB",
-            "/home/eric/Desktop/pattern_rec/TrainINKML/KAIST"
+            #"/home/eric/Desktop/pattern_rec/TrainINKML/expressmatch",
+            "/home/eric/Desktop/pattern_rec/TrainINKML/extension"
+            #"/home/eric/Desktop/pattern_rec/TrainINKML/HAMEX",
+            #"/home/eric/Desktop/pattern_rec/TrainINKML/MathBrush",
+            #"/home/eric/Desktop/pattern_rec/TrainINKML/MfrDB",
+            #"/home/eric/Desktop/pattern_rec/TrainINKML/KAIST"
 
     ]
 }
