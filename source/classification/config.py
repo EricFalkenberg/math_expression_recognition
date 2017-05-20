c662d466669f0e1555dda7fed9d1a2c24391a73d
Eric Falkenberg <exf4789@rit.edu>
2017-05-20 03:45:10 -0400

    HMM -> Random Forest in -h description

--- source/classification/config.py
+++ source/classification/config.py
@@ -15,7 +15,7 @@ kdtree_model = {
 ## HMM Specific Configuration
 random_forest_meta = {
     'program_description': """
-                           Classify isolated handwritten math symbols with an HMM.
+                           Classify isolated handwritten math symbols with a Random Forest.
                            """,
     'class_names' : ['!', '(', ')', '+', '-', '.', '/', '0', '1', '2', '3', '4', '5',
                       '6', '7', '8', '9', '=', 'A', 'B', 'C', 'COMMA', 'E', 'F', 'G',
